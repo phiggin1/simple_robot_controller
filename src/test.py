3d55a8339bb5fdc387b3d91cfde8d518ff785cb9
phiggin1 <phiggin1@umbc.edu>
2021-10-13 12:32:45 -0400

    updated arm target offset

--- src/test.py
+++ src/test.py
@@ -63,20 +63,24 @@ class TestControl:
         loc = []
         for i, pc in enumerate(clusters.clusters):
             num_points = pc.width * pc.height
-            #print("Cluster", i ,": # points ", num_points)
+
+            #print("Cluster "+ str(i) + ": # points " + str(num_points))
             sum_x = 0
-            sum_y = 0
+            #sum_y = 0
+            min_y = 9999.9
             sum_z = 0
             #for all points in the cluster i
             for p in pc2.read_points(pc):
                 sum_x = sum_x + p[0]
-                sum_y = sum_y + p[1]
+                #sum_y = sum_y + p[1]
+                if p[1] < min_y:
+                    min_y = p[1]
                 sum_z = sum_z + p[2]
 
             x = sum_x/num_points
-            y = sum_y/num_points
+            #y = sum_y/num_points
             z = sum_z/num_points
-            loc.append([x,y,z])
+            loc.append([x,min_y,z])
             #print(self.locations)
         del self.locations[:]
         self.locations = loc
@@ -103,18 +107,15 @@ if __name__=='__main__':
 
     buttons = pygame.sprite.Group()
     names = {
-        "What can you tell me about this thing?" : 0, 
         "Can you please describe this object?" : 1, 
-        "What does it look like?" : 2, 
         "What is it used for?" : 3, 
         "Can you tell me more about this?" : 4, 
         "Is this hard or soft?" : 5, 
         "What shape is this?" : 6, 
         "What texture does it have?" : 7, 
         "What color is it?" : 8, 
-        "Thank you!" : 9, 
-        "Would you eat this for lunch?" : 10, 
-        "Can you tell me why?" : 11
+        "what objects would you use to pack a lunch?": 12, 
+        "thank you for yor time you can remove the headset": 13
     }
 
     y = 60
@@ -202,7 +203,7 @@ if __name__=='__main__':
                     if (0<= key < len(tc.locations)):
                         p.header.frame_id = 'kinect2_link'
                         p.point.x = tc.locations[key][0]
-                        p.point.y = tc.locations[key][1]-0.15
+                        p.point.y = tc.locations[key][1]-0.3
                         p.point.z = tc.locations[key][2]
                         print(p)
                         try:
